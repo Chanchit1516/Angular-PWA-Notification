8bef1f492662bdba84780334af8dcd62cdf4cf46
chanchit <chanchit.ler01@gmail.com>
2023-03-06 15:41:15 +0700

    Update code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB9CtCoBmnS-1W-jeMDWGkRhLtQ06h0tLQ",
+    authDomain: "angular-pwa-noti.firebaseapp.com",
+    projectId: "angular-pwa-noti",
+    storageBucket: "angular-pwa-noti.appspot.com",
+    messagingSenderId: "120873258448",
+    appId: "1:120873258448:web:758babf16407377ccdfdd5",
+    measurementId: "G-7H8XZQVR6L"
+  }
 };
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,6 +1,5 @@
-export const environment = {
-  production: false,
-  firebase: {
+importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-app.js'); 
+importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-messaging.js');firebase.initializeApp({ 
     apiKey: "AIzaSyB9CtCoBmnS-1W-jeMDWGkRhLtQ06h0tLQ",
     authDomain: "angular-pwa-noti.firebaseapp.com",
     projectId: "angular-pwa-noti",
@@ -8,5 +7,4 @@ export const environment = {
     messagingSenderId: "120873258448",
     appId: "1:120873258448:web:758babf16407377ccdfdd5",
     measurementId: "G-7H8XZQVR6L"
-  }
-};
+});const messaging = firebase.messaging();
